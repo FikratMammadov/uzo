136f998dd195faa88e098fa0d0c7c8097b0be8c3
Fikrat <fikrat.mammadov.a@asoiu.edu.az>
2023-01-16 03:27:53 +0400

    Added code command and its functionalities

--- cmd/code.go
+++ cmd/code.go
@@ -1,27 +1,82 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"errors"
 	"fmt"
-
 	"github.com/spf13/cobra"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"uzo/utils"
 )
+
+var File string
 
 // codeCmd represents the code command
 var codeCmd = &cobra.Command{
-	Use:   "code",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "code <zip_file_name>",
+	Short: "It will open the directory in Visual Studio Code",
+	Long: `This command will help to open the unzipped folder to Visual Studio Code.
+In order for this command to work, Visual Studio code should be installed in your system`,
+	//Args: cobra.ExactArgs(1),
+	Args: func(cmd *cobra.Command, args []string) error {
+		if File == "" && len(args) < 1 {
+			return errors.New("accepts 1 argument")
+		}
+		return nil
+	},
+	Example: `uzo code demo.zip
+uzo code \Downloads\demo.zip`,
+	DisableFlagsInUseLine: true,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("code called")
+		var fileName string
+		var err error
+		var argument string
+
+		if File != "" {
+			argument = File
+		} else {
+			argument = args[0]
+		}
+
+		fileExists, err := utils.FileExists(argument)
+
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
+		if fileExists {
+			fileName, err = filepath.Abs(argument)
+			if err != nil {
+				fmt.Println(err.Error())
+			}
+		} else {
+			fmt.Printf("File %v does not exist", argument)
+			return
+		}
+
+		wd, err := os.Getwd()
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
+		utils.Unzip(fileName, wd)
+		os.Chdir(utils.FilenameWithoutExtension(fileName))
+
+		wd, err = os.Getwd()
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
+		commandCode := exec.Command("code", wd)
+		err = commandCode.Run()
+
+		if err != nil {
+			fmt.Println("VS Code executable file not fount in %PATH%")
+		}
 	},
 }
 
@@ -32,7 +87,7 @@ func init() {
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// codeCmd.PersistentFlags().String("foo", "", "A help for foo")
+	codeCmd.PersistentFlags().StringVarP(&File, "file", "f", "", "A file name to unzip and open in IDE")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
---
+++ utils/utils.go
@@ -0,0 +1,62 @@
+package utils
+
+import (
+	"archive/zip"
+	"io"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+)
+
+func Unzip(src, dest string) error {
+	r, err := zip.OpenReader(src)
+	if err != nil {
+		return err
+	}
+	defer r.Close()
+
+	for _, f := range r.File {
+		rc, err := f.Open()
+		if err != nil {
+			return err
+		}
+		defer rc.Close()
+
+		path := filepath.Join(dest, f.Name)
+		if f.FileInfo().IsDir() {
+			os.MkdirAll(path, f.Mode())
+		} else {
+			f, err := os.OpenFile(
+				path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+			if err != nil {
+				return err
+			}
+			defer f.Close()
+
+			_, err = io.Copy(f, rc)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func FileExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+
+	return false, err
+}
+
+func FilenameWithoutExtension(fn string) string {
+	return strings.TrimSuffix(fn, path.Ext(fn))
+}
